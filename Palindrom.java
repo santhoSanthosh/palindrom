3341433b313c5476de3901eed28ee0ac0813462b
santhosh <99480228+santhoSanthosh@users.noreply.github.com>
2024-08-21 17:19:33 +0530

    palindrom interview questions

---
+++ Palindrom.java
@@ -0,0 +1,59 @@
+package interview;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class CollectToMapExample {
+	public static void main(String[] args) {
+		String palindromWords = "madama";
+		CollectToMapExample cm = new CollectToMapExample();
+		cm.palindromMatchers(palindromWords);
+	}
+
+	public List<String> palindromMatchers(String input) {
+		String[] data = input.split("");
+		List<String> result = null;
+
+		if (findPalindrom(input)) {
+			return Arrays.asList(input);
+		} else {
+			int exist = data.length;
+			int checkChar = data.length - exist;
+			while (checkChar <= data.length) {
+				for (int i = 0; i <= data.length; i++) {
+					if (i + checkChar <= data.length) {
+						if (findPalindrom(input.substring(i, checkChar + i))) {
+							result.add(input);
+						}
+
+					}
+				}
+				
+				exist--;
+			}
+
+		}
+
+		return result;
+	}
+
+	public Boolean findPalindrom(String input) {
+		String[] data = input.split("");
+		System.out.println(input);
+
+		Boolean palindrom = false;
+		for (int v = 0; v <= data.length - 1; v++) {
+			if (data[data.length - v - 1].equals(data[v])) {
+				palindrom = true;
+			} else {
+				palindrom = false;
+				break;
+			}
+		}
+
+		System.out.println(palindrom);
+
+		return palindrom;
+	}
+}
